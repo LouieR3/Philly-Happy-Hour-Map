a11681153c45a32478de98c1dacdd1a71a2e4352
louier3 <lou3@lourodriguez.com>
2023-09-11 16:37:35 -0400

    start of new map

---
+++ Restaurant Week/readModalForDeals.py
@@ -0,0 +1,107 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from tabulate import tabulate
+from geopy.geocoders import Nominatim
+import time
+import os
+import folium
+import re
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# ------------------------------------------------
+# This script does = Opens each sips bar modal for its information
+# ------------------------------------------------
+
+# url = 'https://centercityphila.org/explore-center-city/ccd-sips/list-view#cavanaugh-s-rittenhouse'
+
+csvName = "RestaurantWeek.csv"
+df = pd.read_csv(csvName)
+
+# driver = webdriver.Chrome()
+
+# Create an empty list to store the extracted "Deals" content
+deals_list = []
+
+# Loop through each row in the DataFrame
+for index, row in df.iterrows():
+    # Get the URL value for each row
+    url = row['Url']
+
+    # Open the URL with the webdriver
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    # Wait for modal to load
+    driver.implicitly_wait(2) 
+
+    # Find modal div
+    modal = driver.find_element(By.CSS_SELECTOR, '.c-modal[data-role="modal-viewport"]')
+    # Find title 
+    title = modal.find_element(By.CSS_SELECTOR, '.c-modal__title')
+    bar = title.text
+    print(bar)
+    # Get content
+    content = modal.find_element(By.CSS_SELECTOR, '.apos-rich-text')
+    deals = content.text
+    print(deals)
+    print()
+    print(content)
+    # Append the extracted "Deals" content to the list
+    deals_list.append(deals)
+    driver.quit()
+fbdgf
+# driver.quit()
+
+# Add the "Deals" content to the DataFrame as a new column
+df['Deals'] = deals_list
+
+# Save the updated DataFrame back to the CSV file with the same name
+df.to_csv(csvName, index=False)
+# df = pd.read_csv('AllSipsOriginal.csv')
+# Initialize empty lists for each deal type
+cocktails = []
+wine = []
+beer = []
+appetizers = []
+
+# Iterate through each row in the DataFrame
+for row in df.itertuples():
+    # Use regular expressions to extract data for each deal type
+    cocktails_match = re.search(r'\$7 Cocktails(.*?)\$6 Wine', row.Deals, re.DOTALL)
+    wine_match = re.search(r'\$6 Wine(.*?)\$5 Beer', row.Deals, re.DOTALL)
+    beer_match = re.search(r'\$5 Beer(.*?)Half-Priced Appetizers', row.Deals, re.DOTALL)
+    appetizers_match = re.search(r'Half-Priced Appetizers(.*?)$', row.Deals, re.DOTALL)
+
+    # Append extracted data to respective lists
+    if cocktails_match:
+        cocktails.append(cocktails_match.group(1).strip())
+    else:
+        cocktails.append(None)
+    if wine_match:
+        wine.append(wine_match.group(1).strip())
+    else:
+        wine.append(None)
+    if beer_match:
+        beer.append(beer_match.group(1).strip())
+    else:
+        beer.append(None)
+    if appetizers_match:
+        appetizers.append(appetizers_match.group(1).strip())
+    else:
+        appetizers.append(None)
+
+# Create a new DataFrame with the extracted data
+# Add the new columns to the DataFrame
+df['Cocktails'] = cocktails
+df['Wine'] = wine
+df['Beer'] = beer
+df['Half-Priced Appetizers'] = appetizers
+
+# Drop the original 'Deals' column
+df.drop(columns=['Deals'], inplace=True)
+
+# Write the updated DataFrame to the same CSV file
+df.to_csv(csvName, index=False)
---
+++ Restaurant Week/scrapeRWData.py
@@ -0,0 +1,128 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from tabulate import tabulate
+from geopy.geocoders import Nominatim
+import time
+import os
+import folium
+import re
+import json
+
+# ------------------------------------------------
+# This script does = Scraps the basic data from the Sips list site and get Lat and Long
+# ------------------------------------------------
+
+html = "https://centercityphila.org/explore-center-city/ccd-restaurant-week/list-view"
+base_html = "https://centercityphila.org"
+source = requests.get(html).text
+soup = BeautifulSoup(source, "lxml")
+
+geolocator = Nominatim(timeout=10, user_agent="my_app") # type: ignore
+bars = []
+
+pages= []
+pager = soup.find('div', class_='c-pager')
+for link in pager.find_all('a'): # type: ignore
+    pages.append(base_html + link['href'])
+
+print(pages)
+
+pattern = r'apos\.maps\["ccd-places"\]\.addMap\((.*?)\)'
+bar_info_list = []
+for page in pages:
+    # Request and parse HTML for each page 
+    page_html = requests.get(page, allow_redirects=False).text
+    soupIter = BeautifulSoup(page_html, 'lxml')
+    # print(soupIter.find_all('tr')[1])
+    for card in soupIter.find_all('tr'):
+        try:
+            title = card.find('a', class_='o-text-link').text.strip("\n        ")
+            url = html + card.find('a', class_='o-card-link')['href']
+            address = card.find('td', attrs={'data-th': 'Address'}).text.strip()
+            # phone = card.find('td', attrs={'data-th': 'Phone'}).text.strip()
+
+            bars.append([title, address, url])
+        except AttributeError:
+            # One of the classes is not present in this <tr> element
+            # Skip this element and continue with the next one
+            continue
+
+# Create dataframe  
+df = pd.DataFrame(bars, columns=['Restaurant Name', 'Address', 'Url'])
+
+mask = df['Address'].str.contains('Philadelphia')
+df = df[mask]
+df = df.reset_index(drop=True)
+df = df.drop_duplicates(subset=['Restaurant Name'])
+print(df)
+
+pattern = r'apos\.maps\["ccd-places"\]\.addMap\((.*?)\)'
+bar_info_list = []
+for page in pages:
+    page_html = requests.get(page, allow_redirects=False).text
+    soupIter = BeautifulSoup(page_html, 'lxml')
+    script_tags = soupIter.find_all('script', text=re.compile(pattern))
+
+    json_data = {}
+    for script_tag in script_tags:
+        lines = script_tag.string.splitlines()
+        for line in lines:
+            if re.match(pattern, line.strip()):
+                # Extract the JSON text from the line
+                start_index = line.find('{')
+                end_index = line.rfind('}') + 1
+                json_text = line[start_index:end_index]
+                json_data = json.loads(json_text)
+                break
+        if json_data is not None:
+            break
+
+    for item in json_data["items"]:
+        title = item.get("title", "")
+        url_website = item.get("urlWebsite", "")
+        if url_website:
+            url_website = url_website.rstrip('/')
+        bar_info = {"Restaurant Name": title, "Restaurant Website": url_website}
+        print(bar_info)
+        bar_info_list.append(bar_info)
+
+new_df = pd.DataFrame(bar_info_list)
+
+
+# Print the resulting DataFrame with the new "Bar Website" column
+print(new_df)
+csvName = "RestaurantWeek.csv"
+# df = pd.read_csv(csvName)
+merged_df = df.merge(new_df, on='Restaurant Name', how='left')
+print(merged_df)
+merged_df.to_csv(csvName, index=False)
+df = merged_df
+MAX_ATTEMPTS = 5
+
+def find_location(row):
+    place = row['Address'].replace("Ben Franklin", "Benjamin Franklin")
+    print(place)
+    attempts = 0
+    while attempts < MAX_ATTEMPTS:
+        try:
+            location = geolocator.geocode(place)
+            # print(location)
+            return location.latitude, location.longitude # type: ignore
+        except:
+            attempts += 1
+            time.sleep(1)
+    print()
+    return None, None
+
+df[['Latitude','Longitude']] = df.apply(find_location, axis="columns", result_type="expand")
+
+print(df)
+
+# Get the current working directory
+# current_directory = os.getcwd()
+
+# Combine the current directory with the filename
+# file_path = os.path.join(current_directory, 'SipsLocations.csv')
+
+df.to_csv(csvName, index=False)
