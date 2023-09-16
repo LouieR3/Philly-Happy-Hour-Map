3d77fb6643ee0df3d7df2bd556a4b7f7f2e3bcb0
louier3 <lou3@lourodriguez.com>
2023-09-16 10:52:23 -0400

    new Res Week csv

---
+++ Restaurant Week/scrapePhotos.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from tabulate import tabulate
+from geopy.geocoders import Nominatim
+import time
+import os
+import folium
+import re
+import json
+
+# ------------------------------------------------
+# This script does = Scraps the basic data from the Sips list site and get Lat and Long
+# ------------------------------------------------
+
+html = "https://centercityphila.org/explore-center-city/ccd-restaurant-week"
+base_html = "https://centercityphila.org"
+source = requests.get(html).text
+soup = BeautifulSoup(source, "lxml")
+
+locations = []
+
+pages= []
+pager = soup.find('div', class_='c-pager')
+for link in pager.find_all('a'): # type: ignore
+    pages.append(base_html + link['href'])
+
+print(pages)
+page_number = 1
+restaurant_info_list = []
+while True:
+    # Create the URL for the current page
+    current_url = f"{html}?page={page_number}"
+    # Request and parse HTML for each page 
+    page_html = requests.get(current_url, allow_redirects=False).text
+    soupIter = BeautifulSoup(page_html, 'lxml')
+    # print(soupIter.find_all('tr')[1])
+    card_media_divs = soupIter.find_all('div', class_='o-card__media')
+    for card_media in card_media_divs:
+        img = card_media.find('img')
+        if img and img['alt'] != '':
+            alt_text = img['alt'].rstrip('*')
+            src_link = base_html + img['src']
+            restaurant_info_list.append({'Restaurant Name': alt_text, 'Photo': src_link})
+            # print({'alt_text': alt_text, 'src_link': src_link})
+    pager = soupIter.find('div', class_='c-pager')
+    next_page_link = pager.find('a', href=f"/explore-center-city/ccd-restaurant-week?page={page_number + 1}")
+    
+    if not next_page_link:
+        break  # No more pages, exit the loop
+    else:
+        page_number += 1
+
+# Create dataframe  
+df1 = pd.DataFrame(restaurant_info_list)
+
+# Read the DataFrame from the CSV file
+df2 = pd.read_csv('RestaurantWeek.csv')
+
+# Merge the two DataFrames on 'Restaurant Name' column
+merged_df = pd.merge(df2, df1, on='Restaurant Name', how='inner')
+# Print the resulting DataFrame with the new "Bar Website" column
+print(df1)
+print(merged_df)
+csvName = "RestaurantWeek2.csv"
+merged_df.to_csv(csvName, index=False)
--- Sips/test2.py
+++ Sips/test2.py
@@ -1,12 +1,13 @@
 import requests
 import json
+from yelpapi import YelpAPI
+import yelpapi
 
-# API key and endpoint
-API_KEY = "XEmkEwex5TbqjOitZJ8xtLXTBMQQb7BrtQdzzw-hJVWwnIUysCwCzXT2X2xsMbSXR7uHxWnRkyyFIh1CxfLr6Ilj_n-xClAG_h5aKabBtAjJLVnZ9mMfdKFWlMvPZHYx"
+# Backup backup API Key
+yelpapiKey = "XEmkEwex5TbqjOitZJ8xtLXTBMQQb7BrtQdzzw-hJVWwnIUysCwCzXT2X2xsMbSXR7uHxWnRkyyFIh1CxfLr6Ilj_n-xClAG_h5aKabBtAjJLVnZ9mMfdKFWlMvPZHYx"
 
-API_HOST = 'https://api.yelp.com'
-SEARCH_PATH = '/v3/businesses/matches' 
-MENU_PATH = '/v3/businesses/{id}/menu'
+yelp = yelpapi.YelpAPI(yelpapiKey)
+
 
 # List of business aliases 
 aliases = ['1518-bar-and-grill-philadelphia', 'cavanaughs-rittenhouse-philadelphia-2']
@@ -14,24 +15,7 @@ aliases = ['1518-bar-and-grill-philadelphia', 'cavanaughs-rittenhouse-philadelph
 # Search for business IDs
 business_ids = []
 for alias in aliases:
-    search_url = f"{API_HOST}{SEARCH_PATH}?name={alias}"
+  response = yelp.business_query(id=alias) # type: ignore
+  print(response)
 
-    response = requests.get(search_url, 
-                            headers={'Authorization': f'Bearer {API_KEY}'})
-    print(response)
-    business = response.json()[0]
-    business_ids.append(business['id'])
-
-# Get menu data
-menu_data = []  
-for business_id in business_ids:
-
-  menu_url = API_HOST + MENU_PATH.format(id=business_id)
-  
-  response = requests.get(menu_url, 
-                          headers={'Authorization': f'Bearer {API_KEY}'})
-                          
-  menu = response.json()
-  menu_data.append(menu)
-
-print(menu_data)
+# print(menu_data)
